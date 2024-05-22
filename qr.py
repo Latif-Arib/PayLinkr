21dcb008e25c269d3231c2be7b908829728a375f
labdul749@gmail.com <labdul749@gmail.com>
2024-05-22 23:13:11 +0530

    initial commit

---
+++ qr.py
@@ -0,0 +1,13 @@
+import qrcode
+
+
+qr_code = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+qr_code.add_data('This is my data.')
+qr_code_image = qr_code.make_image()
+qr_code_image.save('qr_code.png')
